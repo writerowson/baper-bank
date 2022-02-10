e929a2207d082d58befd3535d882ad180d9454aa
coderowson <runurowson@gmail.com>
2022-02-10 09:40:50 +0600

    first commit

---
+++ js/bank.js
@@ -0,0 +1,13 @@
+document.getElementById("login-submit").addEventListener('click', function () {
+    // get user email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value
+    // get password
+    const passwordField = document.getElementById("user-password");
+    const userpassword = passwordField.value
+    // check email and password
+    if (userEmail == "sontan@gmail.com" && userpassword
+        == 'secret') {
+        window.location.href = 'banking.html'
+    }
+})
