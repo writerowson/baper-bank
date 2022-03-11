b16b985d01dadd97900c46425a6dffb13a24a5e0
coderowson <runurowson@gmail.com>
2022-03-12 04:56:52 +0600

    revise

---
+++ module.js
@@ -0,0 +1,18 @@
+// const players = document.getElementsByClassName('player')[0].style.color = 'tomato'
+// // for (const player of players) {
+// //     player.style.border = '1px solid red'
+
+// // }
+
+// const addPlayer = () => {
+//     // const playerContainer = document.getElementById('players')
+//     const player = document.createElement('div')
+// }
+
+// document.getElementById('players').style.color = "pink"
+
+document.querySelector('#player, .player:first-child').style.color = "red"
+
+// document.querySelectorAll('#players, .player').style.color = "red"
+
+secondTitle2.setAttribut('title', 'I added a tooltip')
